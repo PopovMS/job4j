4428ff6abdc92854554815988a46b2b0405be44d
PopovMS <m.popov83@gmail.com>
2018-09-01 16:21:06 +0500

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Popov Mikhail (m.popov83@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Popov Mikhail";
+    String expect = "Echo, echo, echo : Popov Mikhail"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
