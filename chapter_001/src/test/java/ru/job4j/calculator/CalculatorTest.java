e42246875bce53fe0f83c2264da65d3eef7a65ba
PopovMS <m.popov83@gmail.com>
2018-09-16 17:43:14 +0500

    3.1. "Элементарный калькулятор. Добавлены JavaDoc для теста

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -5,7 +5,19 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+
+/**
+ * Test.
+ *
+ * @author Popov Mikhail (m.popov83@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class CalculatorTest {
+    /**
+     * Test метода add.
+     */
     @Test
     public void whenAddOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
@@ -14,6 +26,9 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+    /**
+     * Test метода subtract.
+     */
     @Test
     public void whenSubtract4And4Then2() {
         Calculator calc = new Calculator();
@@ -22,6 +37,9 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+    /**
+     * Test метода div.
+     */
     @Test
     public void whenDiv10On2Tren5() {
         Calculator calc = new Calculator();
@@ -31,6 +49,9 @@ public class CalculatorTest {
         assertThat(result, is(expected));
     }
     @Test
+    /**
+     * Test метода multiple.
+     */
     public void whenMultiple2on8Then16() {
         Calculator calc = new Calculator();
         calc.multiple(2D, 8D);
