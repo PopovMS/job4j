bd7b41a4ffdf5ca5581165977115b1ec9c5c6cad
PopovMS <m.popov83@gmail.com>
2018-09-16 17:29:59 +0500

    3.1. "Элементарный калькулятор. Добавлены JavaDoc.

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,62 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator.
+ *
+ * @author Popov Mikhail (m.popov83@gmail.com)
+ */
+
 public class Calculator {
+
+    // Contains result - cюда записывается результат выполнения методов.
+
     private double result;
+
+    /**
+     * Сложение
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     */
+
     public void add(double first, double second) {
         this.result = first + second;
     }
+
+    /**
+     * Вычитание
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     */
+
     public void subtract(double first, double second) {
         this.result = first - second;
     }
+
+    /**
+     * Деление
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     */
+
     public void div(double first, double second) {
         this.result = first / second;
     }
+
+    /**
+     * Умножение
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     */
+
     public void multiple(double first, double second) {
         this.result = first * second;
     }
+
+    /**
+     * Метод получения результата
+     * @return - результат вычислений
+     */
+
     public double getResult() {
         return this.result;
     }
