1eef10cc977b8d4ea47c531e78a5f031c22fb4f4
PopovMS <m.popov83@gmail.com>
2019-05-02 19:30:04 +0500

    4. Используя шаблон проектирования - стратегию

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -0,0 +1,13 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Mikhail Popov (m.popov83@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Paint {
+    public void draw(Shape shape) {
+        System.out.println(shape.draw());
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Shape.java
@@ -0,0 +1,10 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Mikhail Popov (m.popov83@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public interface Shape {
+    String draw();
+}
