af65e1f16698263d6f5e9eff6929c8e310cae1a6
PopovMS <m.popov83@gmail.com>
2019-07-13 19:50:55 +0500

    1. Singleton.

---
+++ chapter_002/src/main/java/ru/job4j/tracker/TrackerSingleSFF.java
@@ -0,0 +1,67 @@
+package ru.job4j.tracker;
+
+public class TrackerSingleSFF {
+    private static final TrackerSingleSFF INSTANCE = new TrackerSingleSFF();
+    private TrackerSingleSFF() {
+    }
+
+    public static TrackerSingleSFF getInstance() {
+        return INSTANCE;
+    }
+
+    Tracker tracker = new Tracker();
+
+    /**
+     * Метод реализаущий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    /**
+     * Метод реализующий замену ячейки в массиве
+     * @param id искомый id заявки, которая подлежить замене
+     * @param item заявка, которой заменяем
+     * @return результат
+     */
+    public boolean replace(String id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    /**
+     * Метод находит заявку по id, удалеяет ее, и смещает все остальные заявки на одну влево
+     * @param id принимает id заявки
+     * @return возвращает true по удачному завершению
+     */
+    public boolean delete(String id) {
+        return tracker.delete(id);
+    }
+
+    /**
+     * возвращает массив не нулевых заявок
+     * @return массив
+     */
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    /**
+     *возвращает массив элементов со значением параметра key в поле name
+     * первый цикл - поиск одинаковых значений name для выяснения длины
+     * возвращаемого массива
+     * @param key строковае значение
+     * @return массив
+     */
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+    /**
+     * ищет по id заявку
+     * @param id - id искомой заявки
+     * @return возвращает заявку, если она найдена
+     */
+    public Item findById(String id) {
+        return tracker.findById(id);
+    }
+}
