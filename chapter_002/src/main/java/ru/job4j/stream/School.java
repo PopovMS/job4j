31af3b723b375c33f9b42593b3ddd5e9aa99171f
PopovMS <m.popov83@gmail.com>
2021-08-01 11:35:52 +0500

    1. Фильтрация учеников [#110058]

---
+++ chapter_002/src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Класс принимает список List и поведение через предикат. Формирует исходящий
+ * список отфильтрованных елементов
+ * @author Popov Mikhail (m.popov83@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class School {
+    List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream().filter(predict).collect(Collectors.toList());
+    }
+}
